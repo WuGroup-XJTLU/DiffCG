8f0e24f91705a146a2bad51093eb684105d720b9
zhenghaowu <w415146142@gmail.com>
2025-08-13 06:43:18 +0800

    delete

--- diffcg/util/boltzamnn_inversion.py
+++
@@ -1,7 +0,0 @@
-"""Deprecated module: kept for backward compatibility.
-
-Use `diffcg.util.boltzmann_inversion` instead.
-"""
-
-from .boltzmann_inversion import boltzmann_inversion as Boltzmann_Inversion  # noqa: N813
-from .boltzmann_inversion import get_target_from_distribution as get_target_dict  # noqa: F401
